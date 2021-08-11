c8b719803ba52b378731242a5ff6ca056eaa6716
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-11 21:38:28 +0800

    [STSpringBoot][Robert] refact: Removed unneeded constructor and variables and imports

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -6,21 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
     @Autowired
     EmployeeService employeeService;
-    private final List<Employee> employees = new ArrayList<>();
-
-    public EmployeesController() {
-        employees.add(new Employee(1, "Bert", 25, "Male", 100));
-        employees.add(new Employee(2, "Kyle", 25, "Male", 100));
-    }
 
     @GetMapping()
     public List<Employee> getAllEmployees() {
@@ -57,7 +49,4 @@ public class EmployeesController {
     public Employee update(@PathVariable Integer employeeId, @RequestBody Employee employeeUpdate) {
         return employeeService.update(employeeId, employeeUpdate);
     }
-
-
-
 }
