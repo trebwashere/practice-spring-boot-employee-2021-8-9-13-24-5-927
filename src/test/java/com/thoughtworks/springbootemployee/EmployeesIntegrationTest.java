7226a9d85ab408811d2f00644e39dee6e1a340c4
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-14 11:44:46 +0800

    [STSpringBoot][Robert] test: Removed workaround and used mem db for testing with create-drop. Also added test for throwing exception if Employee ID does not exist.

--- src/test/java/com/thoughtworks/springbootemployee/EmployeesIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeesIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee;
 
+import com.thoughtworks.springbootemployee.exceptions.EmployeeNotFoundException;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
@@ -22,6 +23,7 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,37 +39,17 @@ public class EmployeesIntegrationTest {
     @Autowired
     EmployeeService service;
 
-    @Resource
-    EmployeeRepository employeeTestRepo;
-
-    @Resource
-    CompanyRepository companyTestRepo;
-
-    @Autowired
-    EntityManager entityManager;
-
     private Employee employee;
 
     @BeforeEach
     public void setup() {
-        entityManager.joinTransaction();
         employee = new Employee(1, "Bert", 25, 100, "Male");
         service.save(employee);
         employee = new Employee(2, "Bert2", 25, 100, "Female");
         service.save(employee);
     }
 
-    @AfterEach
-    public void tearDown() {
-        entityManager.joinTransaction();
-        employeeTestRepo.deleteAll();
-        companyTestRepo.deleteAll();
-        entityManager.createNativeQuery("ALTER SEQUENCE COMPANY_SEQ RESTART WITH 1").executeUpdate();
-        entityManager.createNativeQuery("ALTER SEQUENCE EMPLOYEE_SEQ RESTART WITH 1").executeUpdate();
-    }
-
     @Test
-    @Transactional
     void should_return_all_employees_when_getAllEmployees_given_all_employees() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/employees/"))
                 .andDo(print())
@@ -77,7 +59,6 @@ public class EmployeesIntegrationTest {
     }
 
     @Test
-    @Transactional
     void should_delete_employee_when_delete_given_employee_id_and_all_employees() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/employees/{id}", 1))
                 .andExpect(status().isOk())
@@ -88,7 +69,6 @@ public class EmployeesIntegrationTest {
     }
 
     @Test
-    @Transactional
     void should_update_employee_when_update_given_employee_information_and_employee_id() throws Exception {
         String employeeStr = "{\n" +
                 "        \"id\": 1,\n" +
@@ -110,7 +90,6 @@ public class EmployeesIntegrationTest {
     }
 
     @Test
-    @Transactional
     void should_create_employee_and_add_to_list_when_create_given_employee_information() throws Exception {
         String employeeStr = "{\n" +
                 "        \"name\": \"BertCreate\",\n" +
@@ -131,7 +110,6 @@ public class EmployeesIntegrationTest {
     }
 
     @Test
-    @Transactional
     void should_return_list_of_employees_based_on_pageIndex_and_pageSize() throws Exception {
         String pageIndex = "1";
         String pageSize = "2";
@@ -142,7 +120,6 @@ public class EmployeesIntegrationTest {
     }
 
     @Test
-    @Transactional
     void should_return_list_of_employees_when_findAllByGender_given_gender() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/employees")
                 .param("gender", "Male"))
@@ -152,7 +129,6 @@ public class EmployeesIntegrationTest {
     }
 
     @Test
-    @Transactional
     void should_return_employee_when_findById_given_employeeId() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/employees/{id}", 1))
                 .andExpect(status().isOk())
@@ -160,5 +136,13 @@ public class EmployeesIntegrationTest {
                 .andExpect(jsonPath("$.age").value(25))
                 .andExpect(jsonPath("$.salary").value(100))
                 .andExpect(jsonPath("$.gender").value("Male"));
+    }
+
+    @Test
+    void should_return_EmployeeNotFoundException_when_findById_given_invalid_employeeId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees/{id}", 3))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof EmployeeNotFoundException))
+                .andExpect(jsonPath("$.message").value("Employee does not exist!"));
     }
 }
