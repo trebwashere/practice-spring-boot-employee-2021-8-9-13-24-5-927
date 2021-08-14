832e5c247d354260614bdbeed0017bbac120683f
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-14 12:15:36 +0800

    [STSpringBoot][Robert] test: Added DirtiestContext to cleanup any data manipulated by the other tests

--- src/test/java/com/thoughtworks/springbootemployee/EmployeesIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeesIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class EmployeesIntegrationTest {
 
     @Autowired
